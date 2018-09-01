b019643b113e5d49c50ad7926e141f938481905e
Fedor Bystrov <augustbystrov@gmail.com>
2018-09-01 23:06:17 +0300

    add route for product/id and product/list

--- workshop/app.py
+++ workshop/app.py
@@ -12,6 +12,10 @@ _repository = init_repository(
     os.getenv('DB_PORT'),
     os.getenv('DB_NAME'))
 
+_product_resource = products.ProductResource(_repository)
+
 api = application = falcon.API()
 api.add_route('/images', images.ImagesResource())
-api.add_route('/product', products.ProductResource(_repository))
+
+api.add_route('/product/list', _product_resource)
+api.add_route('/product/{product_id:int}', _product_resource)
--- workshop/resources/products.py
+++ workshop/resources/products.py
@@ -13,19 +13,29 @@ class ProductResource:
         self._repository = repository
         self._type = Product
 
-    def on_get(self, request: Request, response: Response) -> None:
-        products = self._repository.get_all(self._type)
-
-        product_list = list()
-        for product in products:
-            product_list.append({
+    def on_get(self, request: Request, response: Response, product_id: int = None) -> None:
+        if product_id:
+            product = self._repository.get(self._type, product_id)
+            print(product)
+            response.body = ujson.dumps({
                 'id': product.id,
                 'name': product.name,
                 'creationTime': product.creation_time.isoformat(),
                 'categoryId': product.category_id,
                 'price': str(product.price),
             })
+        else:
+            products = self._repository.get_all(self._type)
+            product_list = list()
+            for product in products:
+                product_list.append({
+                    'id': product.id,
+                    'name': product.name,
+                    'creationTime': product.creation_time.isoformat(),
+                    'categoryId': product.category_id,
+                    'price': str(product.price),
+                })
 
-        response.body = ujson.dumps(product_list)
+            response.body = ujson.dumps(product_list)
+
         response.status = status_codes.HTTP_200
-
