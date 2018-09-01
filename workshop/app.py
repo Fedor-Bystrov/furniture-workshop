a819a23e067788117a32a160030fd5514fdf6a84
Fedor Bystrov <augustbystrov@gmail.com>
2018-09-01 21:26:40 +0300

    clean up app

--- workshop/app.py
+++ workshop/app.py
@@ -1,6 +1,16 @@
+import os
+
 import falcon
 
-from .resources.images import ImagesResource
+from workshop.repository import init_repository
+from workshop.resources.images import ImagesResource
+
+_repository = init_repository(
+    os.getenv('DB_USR'),
+    os.getenv('DB_PASS'),
+    os.getenv('DB_HOST'),
+    os.getenv('DB_PORT'),
+    os.getenv('DB_NAME'))
 
 api = application = falcon.API()
 api.add_route('/images', ImagesResource())
