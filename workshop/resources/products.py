c78e8195d4fc5b10a3fa4616cc2462095961f762
Fedor Bystrov <augustbystrov@gmail.com>
2018-09-02 20:01:24 +0300

    check exceptions if ProductResource.on_get

--- workshop/resources/products.py
+++ workshop/resources/products.py
@@ -1,6 +1,6 @@
 import ujson
 
-from falcon import Request, Response, status_codes
+from falcon import Request, Response, status_codes, HTTPBadRequest
 
 from workshop.model import Product
 from workshop.repository import Repository
@@ -9,7 +9,6 @@ from workshop.repository import Repository
 class ProductListResource:
 
     def __init__(self, repository: Repository) -> None:
-        super().__init__()
         self._repository = repository
         self._type = Product
 
@@ -26,24 +25,31 @@ class ProductListResource:
             })
 
         response.body = ujson.dumps(product_list)
-        response.status = status_codes.HTTP_200
+        response.status = status_codes.HTTP_OK
 
 
 class ProductResource:
+    _CHUNK_SIZE_BYTES = 4096
 
     def __init__(self, repository: Repository) -> None:
-        super().__init__()
         self._repository = repository
         self._type = Product
 
     def on_get(self, request: Request, response: Response, product_id: int) -> None:
-        product = self._repository.get(self._type, product_id)
-        response.body = ujson.dumps({
-            'id': product.id,
-            'name': product.name,
-            'creationTime': product.creation_time.isoformat(),
-            'categoryId': product.category_id,
-            'price': str(product.price),
-        })
+        try:
+            product = self._repository.get(self._type, product_id)
+            if not product:
+                # TODO тут кидать более специфичный exception
+                raise RuntimeError('Product with id = {} not found!'.format(product_id))
 
-        response.status = status_codes.HTTP_200
+            response.body = ujson.dumps({
+                'id': product.id,
+                'name': product.name,
+                'creationTime': product.creation_time.isoformat(),
+                'categoryId': product.category_id,
+                'price': str(product.price),
+            })
+            response.status = status_codes.HTTP_OK
+
+        except RuntimeError:
+            raise HTTPBadRequest()
