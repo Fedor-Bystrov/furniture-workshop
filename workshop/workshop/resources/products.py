6e6926f82fa8d83aafb10a052ab8150f516b7e87
Fedor Bystrov <augustbystrov@gmail.com>
2018-09-14 21:12:45 +0300

    refactor products resource for flask

--- workshop/workshop/resources/products.py
+++ workshop/workshop/resources/products.py
@@ -1,20 +1,16 @@
 import ujson
 
-from falcon import Request, Response, status_codes, HTTPBadRequest
-
 from workshop.model import Product
 from workshop.repository import Repository
 
-_CHUNK_SIZE_BYTES = 4096
 
-
-class ProductListResource:
+class ProductResource:
 
     def __init__(self, repository: Repository) -> None:
         self._repository = repository
         self._type = Product
 
-    def on_get(self, request: Request, response: Response) -> None:
+    def get_product_list(self) -> str:
         products = self._repository.get_all(self._type)
         product_list = list()
         for product in products:
@@ -27,35 +23,22 @@ class ProductListResource:
                 'description': product.short_description
             })
 
-        response.body = ujson.dumps(product_list)
-        response.status = status_codes.HTTP_OK
+        return ujson.dumps(product_list)
 
+    def get_product(self, product_id: int) -> str:
+        product = self._repository.get(self._type, product_id)
+        if not product:
+            # TODO тут кидать более специфичный exception
+            raise RuntimeError('Product with id = {} not found!'.format(product_id))
 
-class ProductResource:
-
-    def __init__(self, repository: Repository) -> None:
-        self._repository = repository
-        self._type = Product
-
-    def on_get(self, request: Request, response: Response, product_id: int) -> None:
-        try:
-            product = self._repository.get(self._type, product_id)
-            if not product:
-                # TODO тут кидать более специфичный exception
-                raise RuntimeError('Product with id = {} not found!'.format(product_id))
-
-            response.body = ujson.dumps({
-                'productId': product.product_id,
-                'name': product.name,
-                'creationTime': product.creation_time.isoformat(),
-                'category': {
-                    'categoryId': product.category_id,
-                    'name': product.category.name,
-                },
-                'price': str(product.price),
-                'description': product.description,
-            })
-            response.status = status_codes.HTTP_OK
-
-        except RuntimeError:
-            raise HTTPBadRequest()
+        return ujson.dumps({
+            'productId': product.product_id,
+            'name': product.name,
+            'creationTime': product.creation_time.isoformat(),
+            'category': {
+                'categoryId': product.category_id,
+                'name': product.category.name,
+            },
+            'price': str(product.price),
+            'description': product.description,
+        })
